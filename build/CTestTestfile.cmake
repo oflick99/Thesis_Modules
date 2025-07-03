8505921ed71fe1b8e8b77aea1eb5e120b01cc3b2
oflick99 <oli.flick@t-online.de>
2025-07-03 13:05:03 +0200

    set_up_1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/oflick/HBK/Team DAQ - Oliver-Flick/241106_Bachelor/Bachelor_Arbeit/Workspace/hbk-opendaq-modules-main
+# Build directory: C:/Users/oflick/HBK/Team DAQ - Oliver-Flick/241106_Bachelor/Bachelor_Arbeit/Workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/boost-build")
+subdirs("_deps/opendaq-build")
+subdirs("utilities")
+subdirs("modules/advanced_recorder_module")
+subdirs("modules/mtig710_device_module")
+subdirs("modules/playback_device_module")
+subdirs("apps")
